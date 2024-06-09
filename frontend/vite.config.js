b913116316f2f21c032d494548ae77e410e1cec9
Rossen <jetso1127@gmail.com>
2024-06-09 16:47:47 +0800

    chore: 更新 package.json 腳本，安裝concurrently套件以支援前後端同時啟動

---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000", // Firebase Hosting 的 URL
+        secure: false, // Firebase Hosting 是 https，因此設為 true
+        // target: "https://rossen-hua.onrender.com/", 
+        // secure: true, // Firebase Hosting 是 https，因此設為 true
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
